dc0cd3ac09b44d3e2569ffe79090aa69b0f3e77d
Nilesh Pandit <nileshpandit009@gmail.com>
2023-03-22 22:37:01 +0530

    Added weather cards to weather container.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import WeatherInfoContainer from "containers/WeatherInfoContainer";
 import "App.css";
 import Dropdown from "containers/Dropdown";
-import { useSelector } from "react-redux";
-import { dataSelector } from "selectors/weatherInfoSelector";
 
 function App() {
-  const weatherData = useSelector(dataSelector);
   return (
     <div>
       <div>
         <h1 className="neon">Weather App</h1>
         <Dropdown />
-        <WeatherInfoContainer weatherData={weatherData} />
+        <WeatherInfoContainer />
       </div>
     </div>
   );
--- src/containers/WeatherInfoContainer.jsx
+++ src/containers/WeatherInfoContainer.jsx
@@ -1,11 +1,75 @@
-import { Typography } from "@mui/material";
+import { Container, createTheme, Grid, Paper, ThemeProvider, Typography } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import { dataSelector } from "selectors/weatherInfoSelector";
+import "containers/styles/WeatherInfoContainer.css";
 
-const WeatherInfoContainer = ({ weatherData }) => {
+const WeatherInfoContainer = () => {
+  const theme = createTheme({
+    components: {
+      MuiPaper: {
+        styleOverrides: {
+          root: {
+            backgroundColor: "#4d4d4d",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between"
+          }
+        }
+      }
+    }
+  });
+
+  const weatherData = useSelector(dataSelector);
+
+  const degreeSymbol = "\u00b0";
+
+  if (!weatherData) return <></>;
+
   const {
-    Headline: { Text: headline }
+    Headline: { Text: headline },
+    DailyForecasts: forecasts
   } = weatherData;
-  return <Typography variant="h4">{headline}</Typography>;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container className="weather-info-container" maxWidth="lg">
+        <Paper elevation={6}>
+          <Typography variant="h5" color={"whitesmoke"}>
+            {headline}
+          </Typography>
+        </Paper>
+
+        <Grid container spacing={2} justifyContent="center" justifyItems={"stretch"} marginTop="2rem">
+          {forecasts &&
+            forecasts.map((forecast, idx) => {
+              const date = new Date(forecast.Date).toDateString();
+              const iconPhrase = forecast.Day.IconPhrase;
+              const minTemp = forecast.Temperature.Minimum;
+              const maxTemp = forecast.Temperature.Maximum;
+              const minTempStr = `${minTemp.Value} ${degreeSymbol}${minTemp.Unit}`;
+              const maxTempStr = `${maxTemp.Value} ${degreeSymbol}${maxTemp.Unit}`;
+
+              return (
+                <Grid item display="flex" alignItems={"stretch"}>
+                  <Paper key={idx} style={{ width: "10rem" }}>
+                    <Typography variant="body1" color={"whitesmoke"} padding="0.5rem">
+                      {date}
+                    </Typography>
+                    <Typography variant="body1" color={"whitesmoke"} padding="0.5rem">
+                      {iconPhrase}
+                    </Typography>
+                    <Typography variant="body1" color={"whitesmoke"} padding="0.5rem">
+                      {minTempStr} / {maxTempStr}
+                    </Typography>
+                  </Paper>
+                </Grid>
+              );
+            })}
+        </Grid>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default WeatherInfoContainer;
